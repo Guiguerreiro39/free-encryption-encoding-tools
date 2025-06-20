fd5d901d05a8edb6191d91e4654deb1241051705
Guilherme Guerreiro <mail@guilhermegr.com>
2025-06-20 18:43:34 +0100

    fix manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,26 +3,26 @@
 import type { MetadataRoute } from "next";
 import { SITE_CONFIG } from "@/seo";
 
-const manifest: MetadataRoute.Manifest = {
-  name: SITE_CONFIG.name,
-  short_name: "CipherToolbox",
-  start_url: "/",
-  display: "standalone",
-  background_color: "#fcf6f3",
-  theme_color: "#b98771",
-  description: SITE_CONFIG.description.default,
-  icons: [
-    {
-      src: "/favicon.ico",
-      sizes: "any",
-      type: "image/x-icon",
-    },
-    {
-      src: "/icon.png",
-      sizes: "500x500",
-      type: "image/png",
-    },
-  ],
-};
-
-export default manifest;
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: SITE_CONFIG.name,
+    short_name: "CipherToolbox",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fcf6f3",
+    theme_color: "#b98771",
+    description: SITE_CONFIG.description.default,
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+      {
+        src: "/icon.png",
+        sizes: "500x500",
+        type: "image/png",
+      },
+    ],
+  };
+}
